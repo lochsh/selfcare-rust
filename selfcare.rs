7632577e8e2364304148eb333fb7ff595cbd5452
Hannah McLaughlin <hannah.mclaughlin@paconsulting.com>
2016-04-06 12:58:35 +0100

    Can now read file into Vec<String>.  Some tidying up to do -- still learning!

--- selfcare.rs
+++ selfcare.rs
@@ -1,23 +1,45 @@
 use std::error::Error;
-use std::fs::File;
-use std::io::BufReader;
-use std::io::Lines;
 use std::io::prelude::*;
+use std::fs::File;
 use std::path::Path;
 
 fn read_lines(file_path: &Path) -> Vec<String> { 
+    let file = open_file(file_path);
+    let content = read_file(&file);
+    let lines = split_lines(&content);
+    lines
+
+}
+
+fn open_file(file_path: &Path) -> File {
     let display = file_path.display();
-    let file = match File::open(file_path) {
-        Err(why) => panic!("couldn't open {}: {}",
-                           display, Error::description(&why)),
+    let mut file = match File::open(&file_path) {
+        Err(why) => panic!("couldn't open {}: {}", display,
+                           Error::description(&why)),
         Ok(file) => file,
     };
-    let reader = BufReader::new(file);
-    let lines: Vec<String> = reader.lines().collect();
-    lines
+    file
 }
+
+fn read_file(mut file: &File) -> String {
+    let mut content = String::new();
+    match file.read_to_string(&mut content) {
+        Err(why) => panic!("couldn't read file: {}",
+                           Error::description(&why)),
+        Ok(_) => print!("{}", content),
+    };
+    content
+}
+
+fn split_lines(string: &String) -> Vec<String> {
+    string
+        .lines()
+        .map(ToOwned::to_owned)
+        .collect()    
+}
+    
 
 fn main() {
     let file_path = Path::new("../self-ca.re/adjectives.txt");
-    println!("{}", read_lines(&file_path));
+    println!("{:?}", read_lines(&file_path));
 }
