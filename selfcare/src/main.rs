0c58599437113e6f9e59371b06c5bc9e24708b87
Hannah McLaughlin <hannah.mclaughlin@paconsulting.com>
2016-04-07 17:36:14 +0100

    Updated following code review on stack exchange.  Have removed unhelpful docstrings, and changed the my functions accept arguments to make them more generic.  I need to learn a bit more about AsRef trait.  My error handling is also better now

--- selfcare/src/main.rs
+++ selfcare/src/main.rs
@@ -7,62 +7,35 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-/// Takes file path, returns a vector of strings, each string a line in file.
-///
-/// # Arguments
-/// * `file_path: &Path` - The path to the file to read
-///
-/// # Return value
-/// Returns `Vec<String>` where each string is a line in the file
-fn read_lines(file_path: &Path) -> Vec<String> { 
+fn read_lines<P>(file_path: P) -> Vec<String> where P: AsRef<Path> { 
     let content = read_file(file_path);
-    split_lines(content)
+    split_lines(&content)
 }
 
-/// Takes file path, returns corresponding `File` object.
-///
-/// # Arguments
-/// * `file_path: &Path` - The path to file to read
-///
-/// # Return value
-/// Returns `File` corresponding to `file_path`
-fn open_file(file_path: &Path) -> File {
-    let display = file_path.display();
-    let file = match File::open(file_path) {
-        Err(why) => panic!("Couldn't open file {}: {}", display,
+fn open_file<P>(file_path: P) -> File where P: AsRef<Path> {
+    let file_path = file_path.as_ref();
+    match File::open(file_path) {
+        Err(why) => panic!("Couldn't open file {}: {}",
+                           file_path.display(),
                            Error::description(&why)),
         Ok(file) => file,
-    };
-    file
+    }
 }
 
-/// Takes `file path, returns string containing the file's content.
-///
-/// # Arguments
-/// * `file_path: &Path` - The path of file to read
-///
-/// # Return value
-/// Returns `String` containing contents of file
-fn read_file(file_path: &Path) -> String {
+fn read_file<P>(file_path: P) -> String where P: AsRef<Path> {
+    let file_path = file_path.as_ref();
     let mut file = open_file(file_path);
-    let display = file_path.display();
     let mut content = String::new();
-    match file.read_to_string(&mut content) {
-        Err(why) => panic!("Couldn't read file {}: {}", display,
-                           Error::description(&why)),
-        Ok(content) => content,
-    };
+
+    if let Err(why) = file.read_to_string(&mut content) {
+        panic!("Couldn't read file {}: {}",
+               file_path.display(),
+               why.description());
+    }
     content
 }
 
-/// Split a string on newlines and return vector of resulting strings.
-///
-/// # Arguments
-/// * `string: String` - string to be split
-///
-/// # Return value
-/// Returns `Vec<String>`
-fn split_lines(string: String) -> Vec<String> {
+fn split_lines(string: &str) -> Vec<String> {
     string
         .lines()
         .map(ToOwned::to_owned)
@@ -73,33 +46,24 @@ fn split_lines(string: String) -> Vec<String> {
 ///
 /// This function does not return the message, but prints it to stdout.
 /// The message is in the format "You are a <adjective> <noun>."
-///
-/// # Arguments
-/// * `adj: Vec<String>` - vector of adjectives
-/// * `nouns: Vec<String` - vector of nouns
-///
-/// # Return value
-/// ()
-fn selfcare(adj: Vec<String>, nouns: Vec<String>) {
-    let adjective = rand::thread_rng().choose(&adj).unwrap();
-    let noun = rand::thread_rng().choose(&nouns).unwrap();
+fn selfcare<S>(adj: &[S], nouns: &[S]) where S: std::fmt::Display {
+    let mut rng = rand::thread_rng();
+    let adjective = rng.choose(adj).expect("No adjectives found!");
+    let noun = rng.choose(nouns).expect("No nouns found!");
     println!("You are a{} {}", adjective, noun)
 }
-    
 
 /// Print random inspiring message in format "You are a <adjective> <noun>".
 ///
 /// Takes command line args `adj_file` and `noun_file`, respectively the paths
 /// to the file containing the adjectives and the containing the nouns.
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
+    let mut args: Vec<_> = env::args().collect();
     if args.len() == 1 {
         args.push("adjectives.txt".to_string());
         args.push("nouns.txt".to_string());
     }
-    let adj_file = Path::new(&args[1]);
-    let noun_file = Path::new(&args[2]);
-    let adj = read_lines(adj_file);
-    let nouns = read_lines(noun_file);
-    selfcare(adj, nouns)
+    let adj = read_lines(&args[1]);
+    let nouns = read_lines(&args[2]);
+    selfcare(&adj, &nouns)
 }
